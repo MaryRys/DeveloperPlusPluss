77f12db47e5ce2b5a4567e447b2164a826d85b5a
Mary Rys <rys.mary@gmail.com>
2018-12-31 12:10:08 -0600

    added delete button for uid tutorials

--- src/components/TutorialsItem/TutorialsItem.js
+++ src/components/TutorialsItem/TutorialsItem.js
@@ -3,6 +3,7 @@ import React from 'react';
 import tutorialShape from '../../helpers/propz/tutorialsShape';
 
 import './TutorialsItem.scss';
+import authRequests from '../../helpers/data/authRequests';
 
 class TutorialsItem extends React.Component {
   static propTypes = {
@@ -11,10 +12,26 @@ class TutorialsItem extends React.Component {
 
   render() {
     const { tutorial } = this.props;
+    const uid = authRequests.getCurrentUid();
+    const makeButtons = () => {
+      if (tutorial.uid === uid) {
+        return (
+            <div>
+              <span className="col">
+                <button className="btn btn-danger">
+                  <i className="far fa-trash-alt"></i>
+                </button>
+              </span>
+            </div>
+        );
+      }
+      return <span className="col-2"></span>;
+    };
     return (
       <li className="tutorials-item text-center">
         <span className="col-6">{tutorial.title}</span>
         <span className="col-6">{tutorial.url}</span>
+        {makeButtons()}
       </li>
     );
   }
