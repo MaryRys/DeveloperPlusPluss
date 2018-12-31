5f6ed282a421a18b6607db3be6c3b9bbe799eea8
Mary Rys <rys.mary@gmail.com>
2018-12-31 13:36:09 -0600

    onChange works

--- src/components/TutorialsForm/TutorialsForm.js
+++ src/components/TutorialsForm/TutorialsForm.js
@@ -1,8 +1,28 @@
 import React from 'react';
 import './TutorialsForm.scss';
 
+const defaultTutotial = {
+  title: '',
+  url: '',
+  uid: '',
+};
+
 class TutorialsForm extends React.Component {
+  state ={
+    newTutorial: defaultTutotial,
+  }
+
+  formFieldStringState = (name, e) => {
+    e.preventDefault();
+    const tempTutorial = { ...this.state.newTutorial };
+    tempTutorial[name] = e.target.value;
+    this.setState({ newTutorial: tempTutorial });
+  }
+
+  titleChange = e => this.formFieldStringState('title', e);
+
   render() {
+    const { newTutorial } = this.state;
     return (
       <div className="tutorials-form col">
         <h2>Add New Tutorial:</h2>
@@ -15,6 +35,8 @@ class TutorialsForm extends React.Component {
               id="title"
               aria-describedby="titleHelp"
               placeholder="Wes Bos- Javascript 30"
+              value={newTutorial.title}
+              onChange={this.titleChange}
             />
           </div>
           <button className="btn btn-success">Save Tutorial</button>
